ffcf1bf1474607b4fa23bfed2ef1ad207d40ee6a
Pamela Rojas <pamela.rojasq@usm.cl>
2022-12-07 17:48:16 -0300

    se agrega archivos bootstrap, html base

--- Neonatologia/core/views.py
+++ Neonatologia/core/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def inicio(request):
+    #return HttpResponse("<h1>Hola Mundo</h1>")
+    return render(request,'core/inicio.html')
